81378463abc0b93398e5c3c9034e136c1aa6b570
p3t3rsCode <peter2796b@gmail.com>
2020-08-01 20:38:55 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">App Name</div>
+
+      <v-spacer></v-spacer>
+      <drop-down-menu :menu-items="menuItems" />
+    </v-app-bar>
+
+    <v-content>
+      <router-view />
+    </v-content>
+    <v-bottom-navigation
+      class="b_container"
+      v-model="bottomNav"
+      fixed
+      bottom
+      background-color="primary"
+    >
+      <v-btn value="recent">
+        <span>Recent</span>
+        <v-icon>mdi-history</v-icon>
+      </v-btn>
+
+      <v-btn value="favorites">
+        <span>Favorites</span>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+      <v-btn value="favorites">
+        <span>Favorites</span>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+      <v-btn value="favorites">
+        <span>Favorites</span>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn value="nearby">
+        <span>Nearby</span>
+        <v-icon>mdi-map-marker</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+  </v-app>
+</template>
+
+<script>
+import DropDownMenu from "@/components/widgets/DropDownMenu";
+export default {
+  name: "App",
+  components: {
+    DropDownMenu
+  },
+  data: () => ({
+    bottomNav: null,
+    menuItems: [
+      {
+        name: "Setting",
+        link: "/settings"
+      },
+      {
+        name: "Logout",
+        link: "/logout"
+      }
+    ]
+  })
+};
+</script>
+<style scoped>
+.b_container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
---
+++ src/components/widgets/DropDownMenu.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-menu top :offset-x="true">
+    <template v-slot:activator="{ on, attrs }">
+      <v-icon v-bind="attrs" v-on="on">mdi-dots-vertical</v-icon>
+    </template>
+
+    <v-list>
+      <v-list-item v-for="(item, index) in menuItems" :key="index">
+        <v-list-item-title>{{ item.name }}</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-menu>
+</template>
+
+<script>
+export default {
+  props: {
+    menuItems: {
+      type: Array,
+      default: () => []
+    }
+  }
+};
+</script>
